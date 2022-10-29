5a1ee74514163f226066b96b0fed6c24589a6027
DiegoJim-13 <d_jimenez13@hotmail.com>
2022-10-28 22:19:01 -0700

    Updating homepage to render

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <head>Alex's Photos!</head>
+      <h1>Alex's Photos!</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a
